7d2e5ee995d1dcc7dbe3f8b85056f64ea23eca8f
Paul D'Antignac <dantignac62@gmail.com>
2025-02-07 06:11:31 -0500

    deleted:    PowerShell/Entra_Intune/win32apps/Intellium EasyPrint/Install-IntelliumEasyPrint copy.ps1 	renamed:    PowerShell/Entra_Intune/Install-IntelliumEasyPrint.ps1 -> PowerShell/Entra_Intune/win32apps/Intellium EasyPrint/Install-IntelliumEasyPrint.ps1 	modified:   PowerShell/Entra_Intune/win32apps/Intellium EasyPrint/IntelliumEasyPrintRequirement.ps1 	deleted:    PowerShell/Entra_Intune/intellium_EasyPrint_4.28.10.exe

--- PowerShell/Entra_Intune/win32apps/Intellium EasyPrint/IntelliumEasyPrintRequirement.ps1
+++ PowerShell/Entra_Intune/win32apps/Intellium EasyPrint/IntelliumEasyPrintRequirement.ps1
@@ -1,10 +1,7 @@
 $EasyPrint = "$($env:ProgramFiles)\intellium EasyPrint\intelliumEasyPrint.exe"
 
-if(Test-Path -Path $EasyPrint)
-{
-    Write-Output 'Intellium EasyPrint found.'
-}
-else 
-{
-Write-Output 'Intellium EasyPrint not found'
+if (Test-Path -Path $EasyPrint) {
+    Write-Output 'Intellium EasyPrint installed.'
+} else {
+    Write-Output 'Intellium EasyPrint not installed'
 }
